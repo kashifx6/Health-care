fdc9bba4d95ed0671c312d2d7c6c3bc15647666b
unknown <mkashifx6@gmail.com>
2023-05-03 05:46:26 +0500

    Navbar Fixed

--- src/components/common/sidebar/sidebar.js
+++ src/components/common/sidebar/sidebar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
-const Sidebar = (props) => {
+const Sidebar = () => {
   const router = useRouter();
   return (
     <>
@@ -41,19 +41,29 @@ const Sidebar = (props) => {
             </li>
             <li>
               <Link
-                href="#"
-                className="flex items-center p-2 text-white rounded-lg "
+                href="/staff/adduser"
+                className={`flex items-center p-2  rounded-lg  ${
+                  router.pathname === "/staff/adduser"
+                    ? "bg-white text-blue-700"
+                    : "hover:bg-blue-600 text-white"
+                }`}
               >
                 <svg
                   aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-white transition duration-75  "
+                  className={`flex-shrink-0 w-6 h-6  ${
+                    router.pathname === "/staff/adduser"
+                      ? "text-blue-700"
+                      : "text-white"
+                  }   transition duration-75  `}
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Item</span>
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Consumer Contract
+                </span>
               </Link>
             </li>
             <li>
@@ -71,7 +81,9 @@ const Sidebar = (props) => {
                   <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
                   <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Item</span>
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Nursing Assessment
+                </span>
                 <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
                   3
                 </span>
@@ -81,8 +93,7 @@ const Sidebar = (props) => {
               <Link
                 href="/staff/users"
                 className={`flex items-center p-2  rounded-lg  ${
-                  router.pathname === "/staff/users" ||
-                  router.pathname === "/staff/adduser"
+                  router.pathname === "/staff/users"
                     ? "bg-white text-blue-700"
                     : "hover:bg-blue-600 text-white"
                 }`}
@@ -90,8 +101,7 @@ const Sidebar = (props) => {
                 <svg
                   aria-hidden="true"
                   className={`flex-shrink-0 w-6 h-6  ${
-                    router.pathname === "/staff/users" ||
-                    router.pathname === "/staff/adduser"
+                    router.pathname === "/staff/users"
                       ? "text-blue-700"
                       : "text-white"
                   }   transition duration-75  `}
@@ -126,7 +136,9 @@ const Sidebar = (props) => {
                     clip-rule="evenodd"
                   ></path>
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Item</span>
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Discharge/Transfer
+                </span>
               </Link>
             </li>
             <li>
---
+++ src/components/nursingAddUser/nursingAddUser.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NursingAddUser = () => {
+  return (
+    <div>NursingAddUser</div>
+  )
+}
+
+export default NursingAddUser
+
